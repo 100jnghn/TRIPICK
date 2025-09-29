0a6c4bb4acc8e796cdb96789a0d1086c995ba80f
Jonghun Baek <gnswhd0415@gmail.com>
2025-09-29 13:19:38 +0900

    FEAT : resources/query prop 추가

--- Tripick/src/com/model/dto/UserDTO.java
+++ Tripick/src/com/model/dto/UserDTO.java
@@ -1,4 +1,77 @@
 package com.model.dto;
 
+import java.util.Date;
+
 public class UserDTO {
+
+    // region FIELDS
+
+    private int userNo;
+    private String id;
+    private String pw;
+    private String nickname;
+    private int age;
+    private Date createdAt;
+    private Date updatedAt;
+
+    // endregion
+    // region GETTER SETTER
+
+    public int getUserNo() {
+        return userNo;
+    }
+
+    public void setUserNo(int userNo) {
+        this.userNo = userNo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    // endregion
 }
