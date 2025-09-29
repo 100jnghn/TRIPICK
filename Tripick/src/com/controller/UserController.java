b2c9a429d6847728db9f599cdc894e9c4beb5138
Jonghun Baek <gnswhd0415@gmail.com>
2025-09-29 14:26:50 +0900

    FEAT : UserDAO 작성ing, UserController에 static 변수 추가

---
+++ Tripick/src/com/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.controller;
+
+public class UserController {
+
+    // region FIELDS
+
+    /*
+    * isLogin : 처음 로그인할 때 false -> true로 변경
+    * myUserNo : 로그인 성공하면 입력한 유저의 user_no 저장
+    * */
+    public static boolean isLogin;
+    public static String myUserNo;
+
+    // endregion
+}
--- Tripick/src/com/model/dao/UserDAO.java
+++ Tripick/src/com/model/dao/UserDAO.java
@@ -1,4 +1,124 @@
 package com.model.dao;
 
+import com.common.DBConnectionMgr;
+import com.controller.UserController;
+import com.model.dto.UserDTO;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static com.common.DBConnectionMgr.getInstance;
+
 public class UserDAO {
+
+    // region PROP
+
+    private Properties prop = new Properties();
+
+    // endregion
+    // region CTOR
+
+    public UserDAO() {
+        try {
+            prop.load(UserDAO.class.getResourceAsStream("/query.properties"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // endregion
+    // region METHODS
+
+    public int insertUser(Connection conn, UserDTO user) {
+        int result = 0;
+        PreparedStatement pstmt = null;
+
+        String sql = "insert into users values (null, ?, ?, ?, ?, now(), null)";
+
+        try {
+            pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, user.getId());
+            pstmt.setString(2, user.getPw());
+            pstmt.setString(3, user.getNickname());
+            pstmt.setInt(4, user.getAge());
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            DBConnectionMgr dbcp = getInstance();
+            dbcp.freeConnection(pstmt);
+        }
+
+        return result;
+    }
+
+    public UserDTO selectOneById(Connection conn, String id) {
+        UserDTO user = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        String sql = "select * from user where id = ?";
+
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, id);
+
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                user = new UserDTO();
+
+                user.setUserNo(rs.getInt("user_no"));
+                user.setId(rs.getString("id"));
+                user.setPw(rs.getString("pw"));
+                user.setNickname(rs.getString("nickname"));
+                user.setAge(rs.getInt("age"));
+                user.setCreatedAt(rs.getDate("created_at"));
+                user.setUpdatedAt(rs.getDate("updated_at"));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            DBConnectionMgr dbcp = getInstance();
+            dbcp.freeConnection(pstmt, rs);
+        }
+
+
+        return user;
+    }
+
+    public int updateId(Connection conn, String id) {
+        int result = 0;
+        PreparedStatement pstmt = null;
+
+        String sql = "update users set id = ? where id = ?";
+
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, id);
+            pstmt.setString(2, UserController.myUserNo);
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            DBConnectionMgr dbcp = getInstance();
+            dbcp.freeConnection(pstmt);
+        }
+
+        return result;
+    }
+
+    // endregion
+
 }
