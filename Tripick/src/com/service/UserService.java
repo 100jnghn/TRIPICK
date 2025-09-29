deaecf1747a89d8d9eb3b7f3651a36da6ded24a4
Choi Min Hyeok <chlals12267@gmail.com>
2025-09-29 15:23:24 +0900

    test 3 commit

--- Tripick/src/com/service/UserService.java
+++ Tripick/src/com/service/UserService.java
@@ -1,29 +1,6 @@
 package com.service;
 
-import com.common.DBConnectionMgr;
-import com.model.dto.ReviewDTO;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
 public class UserService {
-    private final ReviewDTO reviewDTO;
-    Connection conn = null;
-    DBConnectionMgr dbcp = null;
-
-    public UserService() {
-        dbcp = DBConnectionMgr.getInstance();
-        if (dbcp.getConnectionCount() == 0) {
-            try {
-                dbcp.setInitOpenConnections(3);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-
-        reviewDTO = new ReviewDTO();
-    }
 
 
 
