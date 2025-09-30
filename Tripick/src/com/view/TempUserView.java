ec2ec5c9c702ba5608783ba3c24b05126e6561b8
Jonghun Baek <gnswhd0415@gmail.com>
2025-09-30 10:52:25 +0900

    FEAT : View 생성. 테스트를 위해 Delete 추가

---
+++ Tripick/src/com/view/TempUserView.java
@@ -0,0 +1,179 @@
+package com.view;
+
+import com.controller.UserController;
+import com.model.dto.UserDTO;
+
+import java.util.Scanner;
+
+// 로그인 - 회원가입 - 유저 정보 수정 진행 뷰
+public class TempUserView {
+
+    private static Scanner sc = new Scanner(System.in);
+    private UserController userController = new UserController();
+
+    public void startMenu() {
+
+        int choice = 0;
+
+        do {
+            try {
+
+                // 시작 화면 출력
+                displayStartMenu();
+
+                // 로그인 | 회원가입 | 정보 수정 입력
+                choice = sc.nextInt();
+
+                switch (choice) {
+
+                    // 로그인
+                    case 1:
+                        if (UserController.isLogin) {
+                            System.out.println("이미 로그인 상태입니다");
+
+                        } else {
+                            String[] idpw = loginMenu();
+                            userController.login(idpw[0], idpw[1]);
+                        }
+                        break;
+
+                    // 회원가입
+                    case 2:
+                        if (UserController.isLogin) {
+                            System.out.println("이미 로그인 상태입니다.");
+
+                        } else {
+                            UserDTO user = createUserMenu();
+                            userController.signUp(user);
+
+                        }
+                        break;
+
+                    // 회원 정보 수정
+                    case 3:
+                        if (!UserController.isLogin) {
+                            System.out.println("로그인 상태가 아닙니다.");
+
+                        } else {
+                            int updateChoice = updateUserMenu();
+
+                            if(updateChoice == 1) {
+                                System.out.println("변경할 아이디 >> ");
+                                userController.updateId(inputString());
+
+                            }else if (updateChoice == 2) {
+                                System.out.println("변경할 비밀번호 >> ");
+                                userController.updatePw(inputString());
+
+                            } else if (updateChoice == 3) {
+                                System.out.println("변경할 닉네임 >> ");
+                                String nickname = inputString();
+
+                                System.out.println("변경할 나이 >> ");
+                                int age = inputInt();
+
+                                userController.updateInfo(nickname, age);
+                            }
+
+                        }
+                        break;
+                        
+                    case 4:
+                        String id = deleteUserMenu();
+                        userController.deleteUser(id);
+
+                    default:
+                        break;
+                }
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        } while (choice != -1);
+    }
+
+
+
+    private String[] loginMenu() {
+        String[] idpw = new String[2];
+
+        System.out.println("=================================");
+        System.out.println("===========로그인 화면===========");
+        System.out.println("아이디 입력 >> ");
+        idpw[0] = inputString();
+
+        System.out.println("비밀번호 입력 >>");
+        idpw[1] = inputString();
+
+        return idpw;
+    }
+
+    private UserDTO createUserMenu() {
+        UserDTO user = new UserDTO();
+
+        System.out.println("=================================");
+        System.out.println("==========회원가입 화면==========");
+
+        System.out.println("사용할 아이디 >> ");
+        user.setId(inputString());
+
+        System.out.println("사용할 비밀번호 >> ");
+        user.setPw(inputString());
+
+        System.out.println("사용할 닉네임 >> ");
+        user.setNickname(inputString());
+
+        System.out.println("나이 >> ");
+        user.setAge(inputInt());
+
+        return user;
+    }
+
+    private int updateUserMenu() {
+        System.out.println("=================================");
+        System.out.println("==========회원 정보 수정==========");
+        System.out.println("1. 아이디 변경");
+        System.out.println("2. 비밀번호 변경");
+        System.out.println("3. 닉네임, 나이 변경");
+        System.out.println("입력 >> ");
+        int choice = sc.nextInt();
+        return choice;
+    }
+
+    private String deleteUserMenu() {
+        System.out.println("=================================");
+        System.out.println("============유저 삭제============");
+        System.out.println("삭제할 유저 아이디 >> ");
+        String id = inputString();
+
+        return id;
+    }
+
+
+    // region METHODS-DISPLAY
+
+    public void displayStartMenu() {
+        System.out.println("=================================");
+        System.out.println("=============시작화면=============");
+        System.out.println("1. 로그인");
+        System.out.println("2. 회원가입");
+        System.out.println("3. 회원 정보 수정");
+        System.out.print("입력 >> ");
+    }
+
+
+    // endregion
+    // region METHODS-INPUT
+
+    private String inputString() {
+        return sc.next();
+    }
+
+    private int inputInt() {
+        return sc.nextInt();
+    }
+
+
+    // endregion
+}
