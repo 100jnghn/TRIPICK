d41ab4ffc453bffc41907339501e52a6eb341738
Choi Min Hyeok <chlals12267@gmail.com>
2025-09-30 14:37:09 +0900

    view 1차 완성

--- Tripick/src/com/view/TempReviewView.java
+++ Tripick/src/com/view/TempReviewView.java
@@ -1,6 +1,7 @@
 package com.view;
 
 import com.controller.ReviewController;
+import com.controller.UserController;
 import com.model.dto.ReviewDTO;
 
 import java.util.ArrayList;
@@ -10,60 +11,134 @@ import java.util.Scanner;
 public class TempReviewView {
     public Scanner sc = new Scanner(System.in);
 
-    public void test() {
-        ReviewController con = new ReviewController();
-        ReviewDTO reviewDTO = new ReviewDTO();
-        reviewDTO.setReviewNo(891);
-        reviewDTO.setReviewTitle("리뷰 수정 테스트");
-        reviewDTO.setContent("리뷰 수정 테스트, 내용");
-        reviewDTO.setRate(3);
-
-        con.modifyReview(reviewDTO);
-    }
-
 
     public void middleMenu() {
-        displayMiddleMenu();
+        while (true) {
+            displayMiddleMenu();
 
-        int choice = sc.nextInt();
+            int choice = sc.nextInt();
 
-        switch (choice) {
-            case 1:
-                //관관지 메뉴 메소드 호출
-                break;
-            case 2://리뷰 메뉴로 이동
-                reviewMenu();
-                break;
-            case -1://종료
-                return;
+            switch (choice) {
+                case 1:
+                    //관관지 메뉴 메소드 호출
+                    break;
+                case 2://리뷰 메뉴로 이동
+                    reviewMenu();
+                    break;
+                case -1://종료
+                    return;
+            }
         }
 
     }
 
     private void reviewMenu() {
-        displayReviewMenu();
-        ReviewController con = new ReviewController();
+        while (true) {
+            displayReviewMenu();
+            ReviewController con = new ReviewController();
 
-        int choice = sc.nextInt();
-        switch (choice) {
-            case 1://검색
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1://검색
+                    searchReview();
+                    break;
+                case 2://등록
+                    ReviewDTO insertDTO = insertMenu();
+                    con.insertReview(insertDTO);
+                    break;
 
-                break;
-            case 2://등록
-                ReviewDTO insertDTO = insertMenu();
-                con.insertReview(insertDTO);
-                break;
-
-            case 3://수정
-                ReviewDTO modifyDTO = null;
-                break;
-            case 4://삭제
-                break;
+                case 3://수정
+                    ReviewDTO modifyDTO = modifyMenu();
+                    con.modifyReview(modifyDTO);
+                    break;
+                case 4://삭제
+                    int reviewNo = deleteMenu();
+                    con.deleteReview(reviewNo);
+                    break;
+                case -1:
+                    return;
+                default:
+                    System.out.println("잘못된 입력");
+                    break;
+            }
         }
+    }
+
+    private void searchReview() {
+        while (true) {
+            displayReviewSearchMenu();
+            ReviewController con = new ReviewController();
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    int travelNo = searchFromLocMenu();
+                    con.searchFromLoc(travelNo);
+                    break;
+                case 2:
+                    String writer = searchFromWriter();
+                    con.searchFromWriter(writer);
+                    break;
+                case -1:
+                    System.out.println("종료");
+                    return;
+                default:
+                    System.out.println("잘못된 입력");
+                    break;
+            }
+        }
+
+    }
+
+    private String searchFromWriter() {
+        System.out.println("=================================");
+        System.out.println("=============리뷰 검색=============");
+        System.out.print("작성자 닉네임 입력 >> ");
+        return sc.nextLine();
+    }
+
+    private int searchFromLocMenu() {
+        System.out.println("=================================");
+        System.out.println("=============리뷰 검색=============");
+        System.out.print("관광지 번호 입력 >> ");
+        return sc.nextInt();
+    }
+
+    private void displayReviewSearchMenu() {
+        System.out.println("=================================");
+        System.out.println("=============메뉴 선택=============");
+        System.out.println("-1. 종료");
+        System.out.println("1. 관광지로 리뷰 검색");
+        System.out.println("2. 작성자로 리뷰 검색");
+        System.out.print("입력 >> ");
+    }
+
+    private int deleteMenu() {
+        System.out.println("=================================");
+        System.out.println("===========리뷰 삭제 화면===========");
+        System.out.print("삭제할 리뷰 번호 입력 >> ");
+        return sc.nextInt();
+    }
+
+    private ReviewDTO modifyMenu() {
+        ReviewDTO dto = new ReviewDTO();
+        System.out.println("=================================");
+        System.out.println("===========리뷰 수정 화면===========");
+        System.out.print("수정할 리뷰 번호 입력 >> ");
+        dto.setReviewNo(sc.nextInt());
+        sc.nextLine();
+        ReviewController con = new ReviewController();
+        con.readDetailReview(dto.getReviewNo());
+
+        System.out.print("새로운 제목 >> ");
+        dto.setReviewTitle(sc.nextLine());
+        System.out.print("새로운 내용 >> ");
+        dto.setContent(sc.nextLine());
+        return dto;
     }
 
     private ReviewDTO insertMenu() {
         ReviewDTO dto = new ReviewDTO();
+        dto.setReviewNo(UserController.myUserNo);
         System.out.println("=================================");
         System.out.println("===========리뷰 등록 화면===========");
         System.out.print("관광지 번호 입력 >> ");
