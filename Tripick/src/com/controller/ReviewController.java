8aa317ec672aa0ccf1d4ac3fb8f09dffc98fbcc0
Choi Min Hyeok <chlals12267@gmail.com>
2025-09-30 10:52:33 +0900

    merge 전 완료 코드 commit

--- Tripick/src/com/controller/ReviewController.java
+++ Tripick/src/com/controller/ReviewController.java
@@ -2,8 +2,8 @@ package com.controller;
 
 import com.model.dto.ReviewDTO;
 import com.service.ReviewService;
+import com.view.TempReviewView;
 
-import javax.swing.text.View;
 import java.util.ArrayList;
 
 public class ReviewController {
@@ -11,7 +11,7 @@ public class ReviewController {
     private ReviewService reviewService = new ReviewService();
 
     public void searchFromLoc(int travelNo) {
-        View view = new View();
+        TempReviewView view = new TempReviewView();
         ArrayList<ReviewDTO> dtoList = reviewService.searchFromLoc(travelNo);
         if (!dtoList.isEmpty()) {
             view.displayReviewList(dtoList);
@@ -21,7 +21,7 @@ public class ReviewController {
     }
 
     public void searchFromWriter(String nickName) {
-        View view = new View();
+        TempReviewView view = new TempReviewView();
 
         ArrayList<ReviewDTO> dtoList = reviewService.searchFromWriter(nickName);
         if (!dtoList.isEmpty()) {
@@ -32,10 +32,10 @@ public class ReviewController {
     }
 
     public void readDetailReview(int reviewNo) {
-
+        TempReviewView view = new TempReviewView();
         ReviewDTO reviewDTO = reviewService.readDetailReview(reviewNo);
         if (reviewDTO != null) {
-            view.displayReivew(reviewDTO);
+            view.displayReview(reviewDTO);
         } else {
             view.displayNoData();
         }
@@ -44,27 +44,27 @@ public class ReviewController {
     public void insertReview(ReviewDTO reviewDTO) {
         int rs = reviewService.insertReview(reviewDTO);
         if (rs > 0) {
-            new View().displaySuccess("리뷰 생성 성공");
+            new TempReviewView().displaySuccess("리뷰 생성 성공");
         } else {
-            new View().displayError("리뷰 생성 실패");
+            new TempReviewView().displayError("리뷰 생성 실패");
         }
     }
 
     public void modifyReview(ReviewDTO reviewDTO) {
         int rs = reviewService.modifyReview(reviewDTO);
         if (rs > 0) {
-            new View().displaySuccess("리뷰 수정 성공");
+            new TempReviewView().displaySuccess("리뷰 수정 성공");
         } else {
-            new View().displaySuccess("리뷰 수정 실패");
+            new TempReviewView().displaySuccess("리뷰 수정 실패");
         }
     }
 
     public void deleteReview(int reviewNo) {
         int rs = reviewService.deleteReview(reviewNo);
         if (rs > 0) {
-            new View().displaySuccess("리뷰 삭제 성공");
+            new TempReviewView().displaySuccess("리뷰 삭제 성공");
         } else {
-            new View().displayError("리뷰 삭제 실패");
+            new TempReviewView().displayError("리뷰 삭제 실패");
         }
     }
 }
